15015507a0dc7a165ea2d542b6310795af9708e5
Hüseyin Özsüzer <huseyin@bienbase.com>
2022-08-14 02:21:37 +0300

    Transaction aspect i yazıldı ve business sınıfa implemente edildi.

--- TsiErp.Business/Entities/Branch/Services/BranchesAppService.cs
+++ TsiErp.Business/Entities/Branch/Services/BranchesAppService.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Tsi.Application.Contract.Services.EntityFrameworkCore;
 using Tsi.Guids;
 using Tsi.Results;
+using Tsi.Transaction.CrossCuttingConcerns;
 using Tsi.Validation.Validations.FluentValidation.CrossCuttingConcerns;
 using TsiErp.Business.Entities.Branch.Validations;
 using TsiErp.Business.Extensions.ObjectMapping;
@@ -28,6 +29,7 @@ namespace TsiErp.Business.Entities.Branch.Services
             _guidGenerator = guidGenerator;
         }
 
+        //[TransactionScopeAspect(Priority =2)]
         [ValidationAspect(typeof(CreateBranchesValidator), Priority = 1)]
         public async Task<IDataResult<SelectBranchesDto>> CreateAsync(CreateBranchesDto input)
         {
