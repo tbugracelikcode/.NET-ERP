c3eca9a939ce05de3ca5fb92f517d9a276ee9a99
Buğra Çelik <tbugracelik@gmail.com>
2022-08-19 21:19:11 +0300

    Dashboard projesine analiz modelleri eklendi.

---
+++ TsiErp.Dashboard/Models/ProductGroupsAnalysis.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TsiErp.Dashboard.Models
+{
+    public class ProductGroupsAnalysis
+    {
+        /// <summary>
+        /// Ürün Grup ID 
+        /// </summary>
+        public int ProductGroupID { get; set; }
+        /// <summary>
+        /// Ürün Grubu 
+        /// </summary>
+        public string ProductGroupName { get; set; }
+        /// <summary>
+        /// Planlanan Adet
+        /// </summary>
+        public int PlannedQuantity { get; set; }
+        /// <summary>
+        /// Üretilen Adet
+        /// </summary>
+        public int TotalProduction { get; set; }
+        /// <summary>
+        /// Hurda Adet 
+        /// </summary>
+        public int TotalScrap { get; set; }
+        /// <summary>
+        /// Oran 
+        /// </summary>
+        public double Quality { get; set; }
+    }
+}
---
+++ TsiErp.Dashboard/Models/StationOEEAnalysis.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TsiErp.Dashboard.Models
+{
+    public class StationOEEAnalysis
+    {
+        /// <summary>
+        /// Makina ID 
+        /// </summary>
+        public int StationID { get; set; }
+        /// <summary>
+        /// Makina Kodu
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// Vardiya Süresi
+        /// </summary>
+        public decimal ShiftTime { get; set; }
+        /// <summary>
+        /// Planlanan Operasyon Süresi 
+        /// </summary>
+        public decimal PlannedOperationTime { get; set; }
+        /// <summary>
+        /// Gerçekleşen Operasyon Süresi 
+        /// </summary>
+        public decimal OccuredOperationTime { get; set; }
+        /// <summary>
+        /// Hurda Süresi
+        /// </summary>
+        public decimal ScrapTime { get; set; }
+        /// <summary>
+        /// Kullanılabilirlik 
+        /// </summary>
+        public decimal Availability { get; set; }
+        /// <summary>
+        /// Performans
+        /// </summary>
+        public decimal Performance { get; set; }
+        /// <summary>
+        /// Kalite
+        /// </summary>
+        public decimal Quality { get; set; }
+        /// <summary>
+        /// OEE
+        /// </summary>
+        public decimal OEE { get; set; }
+    }
+}
