b8d7ee819207c3438fcea0b1eaad50a1a2194991
emretoplar <emretoplar@gmail.com>
2022-08-25 19:49:30 +0300

    UI silindi

--- TsiErp.Dashboard/Helpers/HelperModels/UygunsuzlukBasliklari.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TsiErp.Dashboard.Helpers.HelperModels
-{
-    public class UygunsuzlukBasliklari
-    {
-        public int ID { get; set; }
-        public string BASLIK { get; set; }
-        public int TUR { get; set; }
-    }
-}
