3af75b7b03c23984c3e9d4244974259dea719f2c
Buğra Çelik <tbugracelik@gmail.com>
2022-09-16 22:41:04 +0300

    erp'de görsel değişiklikler yapıldı

--- TsiErp.ErpUI/Pages/Branch/BranchesListPage.razor.cs
+++ TsiErp.ErpUI/Pages/Branch/BranchesListPage.razor.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.Blazor;
+using Microsoft.AspNetCore.Components;
 using TsiErp.Entities.Entities.Branch.Dtos;
 
 namespace TsiErp.ErpUI.Pages.Branch
@@ -6,6 +7,24 @@ namespace TsiErp.ErpUI.Pages.Branch
     public partial class BranchesListPage
     {
         List<ListBranchesDto> Gridlist = new List<ListBranchesDto>();
+
+        [Parameter]
+        public ButtonRenderStyle SubmitButtonRenderStyle { get; set; }
+                       = ButtonRenderStyle.Secondary;
+
+        [Parameter]
+        public ButtonRenderStyleMode SubmitButtonRenderStyleMode { get; set; }
+                           = ButtonRenderStyleMode.Outline;
+
+        [Parameter] public string SubmitButtonText { get; set; }
+
+        [Parameter]
+        public ButtonRenderStyle CancelButtonRenderStyle { get; set; }
+                           = ButtonRenderStyle.Secondary;
+
+        [Parameter]
+        public ButtonRenderStyleMode CancelButtonRenderStyleMode { get; set; }
+                           = ButtonRenderStyleMode.Outline;
 
         bool PopupVisible = false;
         bool isActiveButton = false;
