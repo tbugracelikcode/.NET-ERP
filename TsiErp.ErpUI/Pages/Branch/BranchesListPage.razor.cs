b309ad3d7ed55cad13fe028767b48bae7807f4f1
Buğra Çelik <tbugracelik@gmail.com>
2022-09-19 10:57:05 +0300

    .

--- TsiErp.ErpUI/Pages/Branch/BranchesListPage.razor.cs
+++ TsiErp.ErpUI/Pages/Branch/BranchesListPage.razor.cs
@@ -1,5 +1,6 @@
 ﻿using DevExpress.Blazor;
 using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.Grids;
 using TsiErp.Entities.Entities.Branch.Dtos;
 
 namespace TsiErp.ErpUI.Pages.Branch
@@ -7,13 +8,14 @@ namespace TsiErp.ErpUI.Pages.Branch
     public partial class BranchesListPage
     {
         List<ListBranchesDto> Gridlist = new List<ListBranchesDto>();
+        SfGrid<ListBranchesDto> Grid;
 
         bool PopupVisible = false;
         bool isActiveButton = false;
 
         protected override async void OnInitialized()
         {
-            //Gridlist = (await BranchesService.GetListAsync(new ListBranchesParameterDto() { IsActive = true })).Data.ToList();
+            Gridlist = (await BranchesService.GetListAsync(new ListBranchesParameterDto() { IsActive = true })).Data.ToList();
         }
 
         void BranchesPopupClosing(PopupClosingEventArgs args)
