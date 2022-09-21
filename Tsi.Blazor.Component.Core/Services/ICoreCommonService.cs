c732cbc6a6b5ee655ace2f3d97cd2403aa4cc06e
Hüseyin Özsüzer <huseyin@bienbase.com>
2022-09-21 16:06:37 +0300

    ERP iyileştirmeler / ButtonEdit componenti eklendi

---
+++ Tsi.Blazor.Component.Core/Services/ICoreCommonService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsi.Blazor.Component.Core.Services
+{
+    public interface ICoreCommonService
+    {
+        public ComponentBase ActiveEditComponent { get; set; }
+    }
+}
--- TsiErp.Entities/Entities/StationGroup/Dtos/ListStationGroupsParameterDto.cs
+++ TsiErp.Entities/Entities/StationGroup/Dtos/ListStationGroupsParameterDto.cs
@@ -12,6 +12,6 @@ namespace TsiErp.Entities.Entities.StationGroup.Dtos
         /// <summary>
         /// Aktiflik
         /// </summary>
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } = true;
     }
 }
