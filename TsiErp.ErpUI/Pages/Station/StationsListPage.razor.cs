923402c8c3e4ff1ee8bb67ca63993afb353f0607
Hüseyin Özsüzer <huseyin@bienbase.com>
2022-09-22 15:05:48 +0300

    İstasyon sayfasına istasyon grup alanı eklendi.

--- TsiErp.ErpUI/Pages/Station/StationsListPage.razor.cs
+++ TsiErp.ErpUI/Pages/Station/StationsListPage.razor.cs
@@ -1,17 +1,31 @@
-﻿using DevExpress.Blazor;
+﻿using Castle.Core.Resource;
+using DevExpress.Blazor;
 using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.Data;
+using Syncfusion.Blazor.DropDowns;
 using Syncfusion.Blazor.Gantt;
 using Syncfusion.Blazor.Grids;
 using Tsi.Core.Utilities.Results;
 using TsiErp.Entities.Entities.Station.Dtos;
+using TsiErp.Entities.Entities.StationGroup;
+using TsiErp.Entities.Entities.StationGroup.Dtos;
 
 namespace TsiErp.ErpUI.Pages.Station
 {
     public partial class StationsListPage
     {
+
+        SfComboBox<string, ListStationGroupsDto> StationGroupComboBox;
+        List<ListStationGroupsDto> StationGroupList = new List<ListStationGroupsDto>();
+
         protected override async void OnInitialized()
         {
             BaseCrudService = StationsService;
+        }
+
+        private async Task GetStationGroupsList()
+        {
+            StationGroupList = (await StationGroupsService.GetListAsync(new ListStationGroupsParameterDto())).Data.ToList();
         }
 
         protected override Task BeforeInsertAsync()
@@ -24,6 +38,31 @@ namespace TsiErp.ErpUI.Pages.Station
             ShowEditPage();
 
             return Task.CompletedTask;
+        }
+
+        public async Task StationGroupFiltering(FilteringEventArgs args)
+        {
+
+            args.PreventDefaultAction = true;
+
+            var pre = new WhereFilter();
+            var predicate = new List<WhereFilter>();
+            predicate.Add(new WhereFilter() { Condition = "or", Field = "Code", value = args.Text, Operator = "contains", IgnoreAccent = true, IgnoreCase = true });
+            predicate.Add(new WhereFilter() { Condition = "or", Field = "Name", value = args.Text, Operator = "contains", IgnoreAccent = true, IgnoreCase = true });
+            pre = WhereFilter.Or(predicate);
+
+            var query = new Query();
+            query = args.Text == "" ? new Query() : new Query().Where(pre);
+
+            await StationGroupComboBox.FilterAsync(StationGroupList, query);
+        }
+
+        public async Task StationGroupOpened(PopupEventArgs args)
+        {
+            if(StationGroupList.Count==0)
+            {
+                await GetStationGroupsList();
+            }
         }
     }
 }
