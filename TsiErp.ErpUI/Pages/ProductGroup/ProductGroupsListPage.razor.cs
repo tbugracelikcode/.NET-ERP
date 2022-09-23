b2da3379585d9b80cf08c33512ca0841475bd807
Buğra Çelik <tbugracelik@gmail.com>
2022-09-23 18:01:55 +0300

    Stok kartları, ürün grubu, sevkiyat adresleri UI bitti

---
+++ TsiErp.ErpUI/Pages/ProductGroup/ProductGroupsListPage.razor.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.Blazor;
+using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.Gantt;
+using Syncfusion.Blazor.Grids;
+using Tsi.Core.Utilities.Results;
+using TsiErp.Entities.Entities.ProductGroup.Dtos;
+
+namespace TsiErp.ErpUI.Pages.ProductGroup
+{
+    public partial class ProductGroupsListPage
+    {
+        protected override async void OnInitialized()
+        {
+            BaseCrudService = ProductGroupsService;
+        }
+
+        protected override Task BeforeInsertAsync()
+        {
+            DataSource = new SelectProductGroupsDto()
+            {
+                IsActive = true
+            };
+
+            ShowEditPage();
+
+            return Task.CompletedTask;
+        }
+    }
+}
