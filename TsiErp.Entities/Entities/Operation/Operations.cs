86d93c62ec0a861fafad73eaa6ab9e905f94f06a
Buğra Çelik <tbugracelik@gmail.com>
2022-09-27 16:31:34 +0300

    Operasyon satır dtoları bitti

--- TsiErp.Entities/Entities/Operation/Operations.cs
+++ TsiErp.Entities/Entities/Operation/Operations.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tsi.Core.Entities.Auditing;;
+using Tsi.Core.Entities.Auditing;
 
 namespace TsiErp.Entities.Entities.Operation
 {
