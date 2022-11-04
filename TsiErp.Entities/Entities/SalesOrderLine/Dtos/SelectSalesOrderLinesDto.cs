1b7044036c5963cbf55c8f58342b0485d9ccbc66
Hüseyin Özsüzer <huseyin@bienbase.com>
2022-11-04 10:32:40 +0300

    Sipariş dto ları oluşturulmaya başlandı. Devam ediyor.

---
+++ TsiErp.Entities/Entities/SalesOrderLine/Dtos/SelectSalesOrderLinesDto.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tsi.Core.Entities.Auditing;
+using TsiErp.Entities.Enums;
+
+namespace TsiErp.Entities.Entities.SalesOrderLine.Dtos
+{
+    public class SelectSalesOrderLinesDto : FullAuditedEntityDto
+    {
+        /// <summary>
+        /// Satış Sipariş ID
+        /// </summary>
+        public Guid SalesOrderID { get; set; }
+        /// <summary>
+        /// Satır Nr
+        /// </summary>
+        public int LineNr { get; set; }
+        /// <summary>
+        /// Stok ID
+        /// </summary>
+        public Guid ProductID { get; set; }
+        /// <summary>
+        /// Stok Kodu
+        /// </summary>
+        public string ProductCode { get; set; }
+        /// <summary>
+        /// Stok Adı
+        /// </summary>
+        public string ProductName { get; set; }
+        /// <summary>
+        /// Birim Set ID
+        /// </summary>
+        public Guid UnitSetID { get; set; }
+        /// <summary>
+        /// Birim Set Kodu
+        /// </summary>
+        public string UnitSetCode { get; set; }
+        /// <summary>
+        /// Bağlı Teklif Satır ID
+        /// </summary>
+        public Guid LikedPropositionLineID { get; set; }
+
+        [Precision(18, 6)]
+        /// <summary>
+        /// Miktar
+        /// </summary>
+        public decimal Quantity { get; set; }
+
+        [Precision(18, 6)]
+        /// <summary>
+        /// Birim Fiyat
+        /// </summary>
+        public decimal UnitPrice { get; set; }
+
+        [Precision(18, 6)]
+        /// <summary>
+        /// İndirim Oranı
+        /// </summary>
+        public decimal DiscountRate { get; set; }
+
+        [Precision(18, 6)]
+        ///<summary>
+        /// İndirim Tutarı
+        /// </summary>
+        public decimal DiscountAmount { get; set; }
+
+        [Precision(18, 6)]
+        /// <summary>
+        /// Satır Tutarı
+        /// </summary>
+        public decimal LineAmount { get; set; }
+
+        [Precision(18, 6)]
+        /// <summary>
+        /// Satır Toplar Tutarı
+        /// </summary>
+        public decimal LineTotalAmount { get; set; }
+        /// <summary>
+        /// Satır Açıklaması
+        /// </summary>
+        public string LineDescription { get; set; }
+        /// <summary>
+        /// KDV Oranı
+        /// </summary>
+        public int VATrate { get; set; }
+
+        [Precision(18, 6)]
+        /// <summary>
+        /// KDV Tutarı
+        /// </summary>
+        public decimal VATamount { get; set; }
+
+        [Precision(18, 6)]
+        /// <summary>
+        /// Kur Turarı
+        /// </summary>
+        public decimal ExchangeRate { get; set; }
+        /// <summary>
+        /// Ödeme Planı ID
+        /// </summary>
+        public Guid PaymentPlanID { get; set; }
+        /// <summary>
+        /// Ödeme Planı Adı
+        /// </summary>
+        public string PaymentPlanName { get; set; }
+        /// <summary>
+        /// Satış Sipariş Satırı Durumu
+        /// </summary>
+        public SalesOrderLineStateEnum SalesOrderLineStateEnum { get; set; }
+        /// <summary>
+        /// Üretim Emri Oluşturulma Tarihi
+        /// </summary>
+        public DateTime? WorkOrderCreationDate { get; set; }
+    }
+}
