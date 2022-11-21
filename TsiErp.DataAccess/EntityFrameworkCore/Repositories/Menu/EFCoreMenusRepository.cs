ae63d787348e2692597f662f690dd8d330bac1b4
Hüseyin Özsüzer <huseyin@bienbase.com>
2022-11-21 17:30:32 +0300

    Menu tablosuna başlandı. Devam ediyor.

---
+++ TsiErp.DataAccess/EntityFrameworkCore/Repositories/Menu/EFCoreMenusRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tsi.Core.Utilities.Services.Business.ServiceRegistrations;
+using Tsi.EntityFrameworkCore.Respositories.EntityFrameworkCore;
+using TsiErp.DataAccess.EntityFrameworkCore.Repositories.OperationUnsuitabilityItem;
+using TsiErp.Entities.Entities.Menu;
+using TsiErp.Entities.Entities.OperationUnsuitabilityItem;
+
+namespace TsiErp.DataAccess.EntityFrameworkCore.Repositories.Menu
+{
+    [ServiceRegistration(typeof(IMenusRepository), DependencyInjectionType.Scoped)]
+    public class EFCoreMenusRepository : EfCoreRepository<Menus>, IMenusRepository
+    {
+        public EFCoreMenusRepository(TsiErpDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
