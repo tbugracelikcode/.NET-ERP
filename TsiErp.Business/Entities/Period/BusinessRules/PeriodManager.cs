5e6ee792508e72adf347dc58dcced81dfc424b9b
Buğra Çelik <tbugracelik@gmail.com>
2023-04-27 15:49:28 +0300

    Period'a queryfactory giydirmesi yapıldı, GetAsync metodunda mapleme mantığı ya da join geliştirmesi yapmak lazım

--- TsiErp.Business/Entities/Period/BusinessRules/PeriodManager.cs
+++ TsiErp.Business/Entities/Period/BusinessRules/PeriodManager.cs
@@ -8,24 +8,5 @@ namespace TsiErp.Business.Entities.Period.BusinessRules
 {
     public class PeriodManager
     {
-        public async Task CodeControl(IPeriodsRepository _repository, string code, IStringLocalizer<PeriodsResource> L)
-        {
-            if (await _repository.AnyAsync(t => t.Code == code))
-            {
-                throw new DuplicateCodeException(L["CodeControlManager"]);
-            }
-        }
-
-        public async Task UpdateControl(IPeriodsRepository _repository, string code, Guid id, Periods entity, IStringLocalizer<PeriodsResource> L)
-        {
-            if (await _repository.AnyAsync(t => t.Id != id && t.Code == code) && entity.Code != code)
-            {
-                throw new DuplicateCodeException(L["UpdateControlManager"]);
-            }
-        }
-
-        public async Task DeleteControl(IPeriodsRepository _repository, Guid id)
-        {
-        }
     }
 }
