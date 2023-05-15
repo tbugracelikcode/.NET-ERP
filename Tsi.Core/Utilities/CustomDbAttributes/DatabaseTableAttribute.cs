5caf25ee4e685d8fad0d1c50d9a133287f759879
Hüseyin Özsüzer <huseyin@bienbase.com>
2023-05-15 10:24:05 +0300

    Stok fişleri menüsü devam ediyor. Resources ve UI henüz başlanmadı.

---
+++ Tsi.Core/Utilities/CustomDbAttributes/DatabaseTableAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tsi.Core.Utilities.CustomDbAttributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class DatabaseTableAttribute : Attribute
+    {
+    }
+}
---
+++ TsiErp.Entities/Entities/StockFiche/Dtos/ListStockFichesDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tsi.Core.Entities.Auditing;
+using TsiErp.Entities.Enums;
+
+namespace TsiErp.Entities.Entities.StockFiche.Dtos
+{
+    public class ListStockFichesDto : FullAuditedEntityDto
+    {
+        /// <summary>
+        /// Satış Fiş No
+        /// </summary>
+        public string FicheNo { get; set; }
+        /// <summary>
+        /// Tarih
+        /// </summary>
+        public DateTime Date_ { get; set; }
+        /// <summary>
+        /// Açıklama
+        /// </summary>
+        public string Description_ { get; set; }
+        /// <summary>
+        /// Fiş Türü
+        /// </summary>
+        public StockFicheTypeEnum FicheType { get; set; }
+        /// <summary>
+        /// Şube Kodu
+        /// </summary>
+        public string BranchCode { get; set; }
+        /// <summary>
+        /// Depo Kodu
+        /// </summary>
+        public string WarehouseCode { get; set; }
+        /// <summary>
+        /// Net Tutar
+        /// </summary>
+        public decimal NetAmount { get; set; }
+    }
+}
