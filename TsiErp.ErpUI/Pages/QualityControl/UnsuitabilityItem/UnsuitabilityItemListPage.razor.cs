a37c3cf79c6a45a008e82388d823789bf8d8f102
Hüseyin Özsüzer <huseyin@bienbase.com>
2023-06-20 18:44:27 +0300

    UnsuitabilityItems sayfası oluşturuldu. Tasarım devam ediyor.

---
+++ TsiErp.ErpUI/Pages/QualityControl/UnsuitabilityItem/UnsuitabilityItemListPage.razor.cs
@@ -0,0 +1,25 @@
+﻿using TsiErp.Entities.Entities.QualityControl.UnsuitabilityItem.Dtos;
+
+namespace TsiErp.ErpUI.Pages.QualityControl.UnsuitabilityItem
+{
+    partial class UnsuitabilityItemListPage
+    {
+        protected override void OnInitialized()
+        {
+            BaseCrudService = UnsuitabilityItemsService;
+            _L = L;
+        }
+
+        protected override Task BeforeInsertAsync()
+        {
+            DataSource = new SelectUnsuitabilityItemsDto()
+            {
+                IsActive = true
+            };
+
+            EditPageVisible = true;
+
+            return Task.CompletedTask;
+        }
+    }
+}
