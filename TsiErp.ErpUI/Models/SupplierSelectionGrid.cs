a0fbe8e446fc09032c14fc0d07a338cd15978b04
Hüseyin Özsüzer <huseyin@bienbase.com>
2024-05-01 18:39:41 +0300

    Satış forecast MRP modalı entegrasyonu yapıldı.

---
+++ TsiErp.ErpUI/Models/SupplierSelectionGrid.cs
@@ -0,0 +1,19 @@
+﻿namespace TsiErp.ErpUI.Models
+{
+    public class SupplierSelectionGrid
+    {
+        public string ProductCode { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public Guid? CurrentAccountID { get; set; }
+
+        public string CurrentAccountName { get; set; }
+
+        public Guid? CurrenyID { get; set; }
+
+        public string CurrenyCode { get; set; }
+
+        public int SupplyDate { get; set; }
+    }
+}
