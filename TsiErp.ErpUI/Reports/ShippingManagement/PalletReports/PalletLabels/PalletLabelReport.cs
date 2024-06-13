c1b2658eacba34ff8c57f9084f697b59138ecbf9
Hüseyin Özsüzer <huseyin@bienbase.com>
2024-06-13 17:52:10 +0300

    Palet etiketi rapor tasarımında düzenlemeler yapıldı.

--- TsiErp.ErpUI/Reports/ShippingManagement/PalletReports/PalletLabels/PalletLabelReport.cs
+++ TsiErp.ErpUI/Reports/ShippingManagement/PalletReports/PalletLabels/PalletLabelReport.cs
@@ -8,9 +8,24 @@ namespace TsiErp.ErpUI.Reports.ShippingManagement.PalletReports.PalletLabels
 {
     public partial class PalletLabelReport : DevExpress.XtraReports.UI.XtraReport
     {
+
+        public string PaletNo { get; set; }
+
         public PalletLabelReport()
         {
             InitializeComponent();
+        }
+
+        private void xrLabel28_BeforePrint(object sender, CancelEventArgs e)
+        {
+            PaletNo = (sender as XRLabel).Text;
+        }
+
+        private void xrBarcode_BeforePrint(object sender, CancelEventArgs e)
+        {
+            XRBarCode barcode = sender as XRBarCode;
+
+            barcode.Text = PaletNo;
         }
     }
 }
