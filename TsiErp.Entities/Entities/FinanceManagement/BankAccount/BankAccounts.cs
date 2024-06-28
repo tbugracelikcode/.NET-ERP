b4cba7ee9085a229f559f5f473814f91e379d6a7
BugraCelik <tbugracelik@gmail.com>
2024-06-28 14:23:05 +0300

    ba cp

--- TsiErp.Entities/Entities/FinanceManagement/BankAccount/BankAccounts.cs
+++ TsiErp.Entities/Entities/FinanceManagement/BankAccount/BankAccounts.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tsi.Core.Entities.Auditing;
+﻿using Tsi.Core.Entities.Auditing;
 using Tsi.Core.Utilities.SqlDataTypeMappingUtilities;
 
 namespace TsiErp.Entities.Entities.FinanceManagement.BankAccount
